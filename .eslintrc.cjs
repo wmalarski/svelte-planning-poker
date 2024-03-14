51cecbfd528eaeadbb49e940e6e9548917c92ca8
Wojciech Malarski <w.malarski@gmail.com>
2024-03-14 20:28:10 +0100

    setup eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,12 +3,17 @@ module.exports = {
 	root: true,
 	extends: [
 		'eslint:recommended',
+		"plugin:import/typescript",
+		"plugin:jsx-a11y/recommended",
+		"plugin:promise/recommended",
 		'plugin:@typescript-eslint/recommended',
 		'plugin:svelte/recommended',
-		'prettier'
+		"plugin:perfectionist/recommended-natural",
+		"plugin:tailwindcss/recommended",
+		'plugin:prettier/recommended',
 	],
 	parser: '@typescript-eslint/parser',
-	plugins: ['@typescript-eslint'],
+	plugins: ['@typescript-eslint', "perfectionist"],
 	parserOptions: {
 		sourceType: 'module',
 		ecmaVersion: 2020,
@@ -27,5 +32,27 @@ module.exports = {
 				parser: '@typescript-eslint/parser'
 			}
 		}
-	]
+	],
+	rules: {
+		"no-await-in-loop": "error",
+		"no-constant-binary-expression": "error",
+		"no-self-compare": "error",
+		"no-use-before-define": "error",
+		"curly": "error",
+		"eqeqeq": ["error", "smart"],
+		"max-params": ["error", 3],
+		"no-console": "warn",
+		"no-lonely-if": "error",
+		"no-unneeded-ternary": "error",
+		"prefer-arrow-callback": "error",
+		"prefer-const": "error",
+		"require-await": "error",
+		"@typescript-eslint/array-type": "error",
+		"@typescript-eslint/consistent-type-imports": "error",
+		"@typescript-eslint/consistent-type-definitions": ["error", "type"],
+		"@typescript-eslint/no-unused-expressions": "error",
+		"prettier/prettier": ["error", { "endOfLine": "auto" }],
+		"tailwindcss/no-custom-classname": "off",
+		"jsx-a11y/label-has-associated-control": "off"	
+	}
 };
